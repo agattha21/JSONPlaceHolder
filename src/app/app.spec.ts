0bf96bd383821e9ffd08adf76a6fdd0636723720
Agatha Nunes da Silva <agathanunes.contato@gmail.com>
2026-09-17 09:59:03 -0300

    JSONPlaceHolder Angular

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [provideHttpClient()],
     }).compileComponents();
   });
 
@@ -13,11 +15,11 @@ describe('App', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
+  
   it('should render title', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, JSONPlaceHolder');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Cadastro de Tarefa');
   });
 });
